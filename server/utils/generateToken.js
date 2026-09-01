9db17eb29ba023c616936c29a7f123c374f81ab3
arpan031 <arpanghosh4617@gmail.com>
2026-09-01 21:04:59 +0530

    Update generateToken.js

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -1,1 +1,16 @@
+const jwt = require("jsonwebtoken");
 
+const generateToken = (employee) => {
+  return jwt.sign(
+    {
+      id: employee._id,
+      role: employee.role,
+    },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: "7d",
+    }
+  );
+};
+
+module.exports = generateToken;
