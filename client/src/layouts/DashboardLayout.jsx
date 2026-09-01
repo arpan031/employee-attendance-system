25f89bac7a0db5465eb0449a7c10e6431b2a5422
arpan031 <arpanghosh4617@gmail.com>
2026-09-01 21:48:11 +0530

    Add DashboardLayout component with Sidebar and Navbar

--- client/src/layouts/DashboardLayout.jsx
+++ client/src/layouts/DashboardLayout.jsx
@@ -1,1 +1,24 @@
+import {
+  Outlet,
+} from "react-router-dom";
 
+import Sidebar from "../components/Sidebar";
+import Navbar from "../components/Navbar";
+
+const DashboardLayout = () => {
+  return (
+    <div className="dashboard-layout">
+      <Sidebar />
+
+      <div className="main-content">
+        <Navbar />
+
+        <main className="page-content">
+          <Outlet />
+        </main>
+      </div>
+    </div>
+  );
+};
+
+export default DashboardLayout;
