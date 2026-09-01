e5e0978e2b60b88a02f801146c765edd41d42697
arpan031 <arpanghosh4617@gmail.com>
2026-09-01 21:13:16 +0530

    Update attendanceRoutes.js

--- server/routes/attendanceRoutes.js
+++ server/routes/attendanceRoutes.js
@@ -1,1 +1,29 @@
+const express = require("express");
 
+const protect = require("../middleware/authMiddleware");
+
+const {
+  checkIn,
+  checkOut,
+  getTodayAttendance,
+  getMyAttendance,
+} = require("../controllers/attendanceController");
+
+const router = express.Router();
+
+router.use(protect);
+
+router.post("/check-in", checkIn);
+router.post("/check-out", checkOut);
+
+router.get(
+  "/today",
+  getTodayAttendance
+);
+
+router.get(
+  "/my",
+  getMyAttendance
+);
+
+module.exports = router;
