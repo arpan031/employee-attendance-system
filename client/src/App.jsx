07872c62d3721e3b2da74b7c79e49a6653ea893f
arpan031 <arpanghosh4617@gmail.com>
2026-09-01 21:53:14 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,1 +1,95 @@
+import {
+  BrowserRouter,
+  Navigate,
+  Route,
+  Routes,
+} from "react-router-dom";
 
+import {
+  AuthProvider,
+} from "./context/AuthContext";
+
+import ProtectedRoute from "./components/ProtectedRoute";
+
+import DashboardLayout from "./layouts/DashboardLayout";
+
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
+import EmployeeDashboard from "./pages/employee/EmployeeDashboard";
+import HRDashboard from "./pages/hr/HRDashboard";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <AuthProvider>
+        <Routes>
+          <Route
+            path="/login"
+            element={<Login />}
+          />
+
+          <Route
+            path="/register"
+            element={<Register />}
+          />
+
+          <Route element={<ProtectedRoute />}>
+            <Route
+              element={
+                <DashboardLayout />
+              }
+            >
+              <Route
+                path="/dashboard"
+                element={
+                  <EmployeeDashboard />
+                }
+              />
+
+              <Route
+                path="/hr"
+                element={
+                  <ProtectedRoute
+                    allowedRoles={[
+                      "hr",
+                    ]}
+                  />
+                }
+              >
+                <Route
+                  index
+                  element={
+                    <HRDashboard />
+                  }
+                />
+              </Route>
+            </Route>
+          </Route>
+
+          <Route
+            path="/"
+            element={
+              <Navigate
+                to="/dashboard"
+                replace
+              />
+            }
+          />
+
+          <Route
+            path="*"
+            element={
+              <Navigate
+                to="/dashboard"
+                replace
+              />
+            }
+          />
+        </Routes>
+      </AuthProvider>
+    </BrowserRouter>
+  );
+};
+
+export default App;
