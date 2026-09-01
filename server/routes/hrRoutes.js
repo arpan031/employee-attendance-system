2698211091d8df604cccfb48cad4139d7d3e51b3
arpan031 <arpanghosh4617@gmail.com>
2026-09-01 21:37:06 +0530

    Update hrRoutes.js

--- server/routes/hrRoutes.js
+++ server/routes/hrRoutes.js
@@ -1,1 +1,38 @@
+const express = require("express");
 
+const protect = require("../middleware/authMiddleware");
+const authorize = require("../middleware/roleMiddleware");
+
+const {
+  getDashboard,
+  getEmployees,
+  getAllAttendance,
+  updateEmployeeStatus,
+} = require("../controllers/hrController");
+
+const router = express.Router();
+
+router.use(protect);
+router.use(authorize("hr"));
+
+router.get(
+  "/dashboard",
+  getDashboard
+);
+
+router.get(
+  "/employees",
+  getEmployees
+);
+
+router.get(
+  "/attendance",
+  getAllAttendance
+);
+
+router.patch(
+  "/employees/:id/status",
+  updateEmployeeStatus
+);
+
+module.exports = router;
