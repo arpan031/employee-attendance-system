183b493f6147c22de08bf87c24c4e9d05e7c337f
arpan031 <arpanghosh4617@gmail.com>
2026-09-01 21:07:48 +0530

    Update attendanceService.js

--- server/services/attendanceService.js
+++ server/services/attendanceService.js
@@ -1,1 +1,51 @@
+const calculateWorkingMinutes = (
+  checkIn,
+  checkOut
+) => {
+  if (!checkIn || !checkOut) {
+    return 0;
+  }
 
+  return Math.floor(
+    (new Date(checkOut) -
+      new Date(checkIn)) /
+      (1000 * 60)
+  );
+};
+
+const calculateStatus = (checkIn) => {
+  const date = new Date(checkIn);
+
+  const hour = date.getHours();
+  const minute = date.getMinutes();
+
+  const totalMinutes =
+    hour * 60 + minute;
+
+  const officeStart = 9 * 60;
+
+  if (totalMinutes <= officeStart) {
+    return "Present";
+  }
+
+  if (totalMinutes <= officeStart + 30) {
+    return "Late";
+  }
+
+  return "Late";
+};
+
+const calculateOvertime = (workingMinutes) => {
+  const standardMinutes = 8 * 60;
+
+  return Math.max(
+    0,
+    workingMinutes - standardMinutes
+  );
+};
+
+module.exports = {
+  calculateWorkingMinutes,
+  calculateStatus,
+  calculateOvertime,
+};
