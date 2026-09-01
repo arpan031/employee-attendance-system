3f6a419a1d834cfb11cc7285675a2a9625cba785
arpan031 <arpanghosh4617@gmail.com>
2026-09-01 21:50:03 +0530

    Update StatCard.jsx

--- client/src/components/StatCard.jsx
+++ client/src/components/StatCard.jsx
@@ -1,1 +1,28 @@
+const StatCard = ({
+  title,
+  value,
+  icon: Icon,
+  description,
+}) => {
+  return (
+    <div className="stat-card">
+      <div className="stat-card-top">
+        <div className="stat-icon">
+          <Icon size={22} />
+        </div>
+      </div>
 
+      <p>{title}</p>
+
+      <h3>{value}</h3>
+
+      {description && (
+        <span>
+          {description}
+        </span>
+      )}
+    </div>
+  );
+};
+
+export default StatCard;
