0bcd223cd4fafd760b08e1d42db04b2cc0217d37
arpan031 <arpanghosh4617@gmail.com>
2026-09-01 21:07:02 +0530

    Update roleMiddleware.js

--- server/middleware/roleMiddleware.js
+++ server/middleware/roleMiddleware.js
@@ -1,1 +1,19 @@
+const authorize = (...roles) => {
+  return (req, res, next) => {
+    if (!req.employee) {
+      return res.status(401).json({
+        message: "Authentication required",
+      });
+    }
 
+    if (!roles.includes(req.employee.role)) {
+      return res.status(403).json({
+        message: "Access denied",
+      });
+    }
+
+    next();
+  };
+};
+
+module.exports = authorize;
