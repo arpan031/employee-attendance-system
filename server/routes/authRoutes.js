559fa8261ff9e0040ef2af75d0bfeb68cbe53306
arpan031 <arpanghosh4617@gmail.com>
2026-09-02 14:30:09 +0530

    Update authRoutes.js

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -3,11 +3,42 @@ const express = require("express");
 const {
   register,
   login,
+  getMe
 } = require("../controllers/authController");
+
+const protect = require("../middleware/authMiddleware");
+
+const validate = require("../middleware/validateMiddleware");
+
+const {
+  registerValidator,
+  loginValidator
+} = require("../validators/authValidator");
 
 const router = express.Router();
 
-router.post("/register", register);
-router.post("/login", login);
+/* Public Routes */
+
+router.post(
+  "/register",
+  registerValidator,
+  validate,
+  register
+);
+
+router.post(
+  "/login",
+  loginValidator,
+  validate,
+  login
+);
+
+/* Protected Routes */
+
+router.get(
+  "/me",
+  protect,
+  getMe
+);
 
 module.exports = router;
