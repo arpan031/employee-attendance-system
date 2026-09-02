af683c81ff094d4e0ff32af1eece787c2e89aa81
arpan031 <arpanghosh4617@gmail.com>
2026-09-02 11:35:28 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -6,13 +6,22 @@ const connectDB = require("./config/db");
 const PORT = process.env.PORT || 5000;
 
 const startServer = async () => {
-  await connectDB();
+  try {
+    await connectDB();
 
-  app.listen(PORT, () => {
-    console.log(
-      `Server running on http://localhost:${PORT}`
+    app.listen(PORT, () => {
+      console.log(
+        `Server running on http://localhost:${PORT}`
+      );
+    });
+  } catch (error) {
+    console.error(
+      "Unable to start server:",
+      error.message
     );
-  });
+
+    process.exit(1);
+  }
 };
 
 startServer();
