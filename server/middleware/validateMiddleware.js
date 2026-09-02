45e98257d1c39dc28aeec10526bde49a4fbd8f0d
arpan031 <arpanghosh4617@gmail.com>
2026-09-02 11:47:42 +0530

    Update validateMiddleware.js

--- server/middleware/validateMiddleware.js
+++ server/middleware/validateMiddleware.js
@@ -1,1 +1,22 @@
+const {
+  validationResult
+} = require("express-validator");
 
+const validate = (req, res, next) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      success: false,
+      message: "Validation failed",
+      errors: errors.array().map((error) => ({
+        field: error.path,
+        message: error.msg
+      }))
+    });
+  }
+
+  next();
+};
+
+module.exports = validate;
