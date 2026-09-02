cd9a69b492ec5dfbd8e88171c458e8c079c2f9e3
arpan031 <arpanghosh4617@gmail.com>
2026-09-02 11:27:03 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,1 +1,25 @@
+require("dotenv").config();
 
+const app = require("../server/app");
+const connectDB = require("../server/config/db");
+
+let connectionPromise;
+
+module.exports = async (req, res) => {
+  try {
+    if (!connectionPromise) {
+      connectionPromise = connectDB();
+    }
+
+    await connectionPromise;
+
+    return app(req, res);
+  } catch (error) {
+    console.error("Vercel API error:", error);
+
+    return res.status(500).json({
+      success: false,
+      message: "Server connection failed"
+    });
+  }
+};
